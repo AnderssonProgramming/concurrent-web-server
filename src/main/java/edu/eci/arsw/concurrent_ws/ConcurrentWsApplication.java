3acbc0bfe3fb8f43010059d518a9fde41168e786
AnderProgramming <158221956+AnderssonProgramming@users.noreply.github.com>
2025-06-25 21:45:56 -0500

    feat: add concurrent-web-server project scaffolding

---
+++ src/main/java/edu/eci/arsw/concurrent_ws/ConcurrentWsApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.concurrent_ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrentWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrentWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/arsw/concurrent_ws/ConcurrentWsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.concurrent_ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrentWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
